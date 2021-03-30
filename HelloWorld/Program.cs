baf95ef736c80b2f37c42e6be75d8309424a12bf
marcos <marcoscarpato@hotmail.com>
2021-03-29 21:00:59 -0300

    Primeiro Commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+static class Program
+{
+    private const int anoAtual = 2021;
+    private const int idadeMinima = 18;
+    
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Qual ano você nasceu?");
+        var anoNascimento = Console.ReadLine();
+        var isIdadeValida = VerificaIdade(anoNascimento);
+
+        if (isIdadeValida)
+        {
+            Console.WriteLine("Você pode comprar bebida alcoolica");
+        }
+        else
+        {
+            Console.WriteLine("Você não pode comprar bebida alcoolica");
+        }
+    }
+
+    private static bool VerificaIdade(string anoNascimento)
+    {
+        return false;
+    }
+
+}
