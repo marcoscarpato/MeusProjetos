b0050207d52ee9c834e5688e08eb86a80b4fe3cf
marcos <marcoscarpato@hotmail.com>
2021-03-30 19:57:07 -0300

    criaçao projeto contador

---
+++ Contador/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Contador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
