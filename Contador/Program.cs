7d024403f66cf598664da46e40defe307cd25773
marcos <marcoscarpato@hotmail.com>
2021-03-30 21:18:51 -0300

    finalizaçao projeto contador

--- Contador/Program.cs
+++ Contador/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
 
 namespace Contador
 {
-    class Program
+    static class Program
     {
+        private const int index = 0;
+
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bem vindo ao Contador Mil Grau");
+            Console.WriteLine("Quer contar até que numero?");
+            var numeroEscolhido = Console.ReadLine();
+            var valorValido = ValidaValor(numeroEscolhido);
+            Resolucao(valorValido);
+            Console.WriteLine("pressione qualquer tecla para sair");
+            Console.ReadKey();
+        }
+
+        static int ValidaValor(string numeroEscolhido)
+
+        {
+            
+            try
+
+            {
+                var valorConvertido = int.Parse(numeroEscolhido);
+                return valorConvertido;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Porfavor digite apenas numeros");
+                var numeroNovo = Console.ReadLine();
+                return ValidaValor(numeroNovo);
+            }
+        }
+
+        static void Resolucao(int valorFinal)
+
+        {
+            var index = 0;
+            Console.WriteLine("Eae chavao, sua contgem vai começar");
+            
+            while (index <= valorFinal)
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+            
         }
     }
 }
