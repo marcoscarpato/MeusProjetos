e7b07420565d6ecaa571e37db37027931b6aae37
marcos <marcoscarpato@hotmail.com>
2021-04-05 14:12:51 -0300

    começoCalculadora

---
+++ CalculadoraPadaria/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculadoraPadaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
